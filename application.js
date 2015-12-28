84f5531c235fc8a51124520f7994ea51ac66ffe9
Michael Baker <michaeltbaker@gmail.com>
2015-12-28 14:39:34 -0600

    Adds the first section

--- application.js
+++ application.js
@@ -1,10 +1,30 @@
+var RE = React.createElement;
+
 var Sections = [
     {
-        "displayName": "Programming Languages: Application and Interpretation",
-        "tags":        ["programming_languages"],
-        "description": "A thing",
-        "links":       [
-            { "text": "How to study programming languages", "url": "https://www.youtube.com/watch?v=3N__tvmZrzc" }
+        displayName: "Programming Languages: Application and Interpretation",
+        tags:        ["programming_languages"],
+        takeAway:    "A simple, precise framework for understanding programming language features.",
+        description: [
+            "These are the materials from a course at Brown university on the subject of understanding and implementing programming languages. This course presents programming languages as the composition of atomic programming elements, mechanisms for combining those elements, and a set of more sophisticated constructs that get simplified into the core atomic and comining constructs.",
+            "I highly recommend watching the introductory video \"How to study programming languages\" first."
+        ],
+        links: [
+            {
+                type: "video",
+                text: "How to study programming languages",
+                url:  "https://www.youtube.com/watch?v=3N__tvmZrzc"
+            },
+            {
+                type: "website",
+                text: "The Book",
+                url:  "http://cs.brown.edu/courses/cs173/2012/book/index.html"
+            },
+            {
+                type: "website",
+                text: "All Course Materials",
+                url:  "http://cs.brown.edu/courses/cs173/"
+            }
         ]
     }
 ]
@@ -15,20 +35,27 @@ var TagDisplayNames = {
 
 var Application = React.createClass({
     render: function() {
-        return React.createElement('div', { className: "Application" }, "hello there")
+        return RE('div', { className: "Application" },
+            _.map(this.props.sections, function(s) { return RE(Section, s) })
+        )
     }
 })
 
 var Section = React.createClass({
     render: function() {
-        return React.createElement("div", { className: "Section" }, [
-            "hello",
-            "world"
+        return RE("div", { className: "Section" }, [
+            RE("h2", null, this.props.displayName),
+            _.map(this.props.tags, function(t) { return RE('div', null, t) }),
+            RE("p", null, this.props.takeAway),
+            _.map(this.props.description, function(p) { return RE("p", null, p) }),
+            RE("ul", null, _.map(this.props.links, function(l) {
+                return RE('li', null, RE('a', { href: l.url }, l.text ))
+            })),
         ])
     }
 })
 
 ReactDOM.render(
-    React.createElement(Application, null),
+    React.createElement(Application, { sections: Sections }),
     document.body
 );
